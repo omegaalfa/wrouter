7be4cd2b717546b5f2ed68f874c64f02605caef9
omegaalfa <omegaalfa.wr3@gmail.com>
2024-06-22 13:22:21 -0300

    Update Wrouter.php

--- src/Wrouter.php
+++ src/Wrouter.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Omegaalfa\Wrouter;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 
