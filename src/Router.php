45dc7f5c7089d8b65c6f482552ffca1d17427832
omegaalfa <omegaalfa.wr3@gmail.com>
2024-06-22 11:58:46 -0300

    Update Router.php

--- src/Router.php
+++ src/Router.php
@@ -2,143 +2,62 @@
 
 declare(strict_types = 1);
 
-namespace Omegaalfa\Router;
+namespace Omegaalfa\Wrouter;
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use RuntimeException;
+use Psr\Http\Message\ServerRequestInterface;
 
 class Router extends TreeRouter
 {
-	/**
-	 * @var RequestInterface
-	 */
-	protected RequestInterface $request;
 
 	/**
 	 * @var ResponseInterface
 	 */
 	protected ResponseInterface $response;
 
-	/**
-	 * @var string|null
-	 */
-	protected ?string $group = null;
 
 	/**
-	 * @param  RequestInterface   $request
 	 * @param  ResponseInterface  $response
 	 */
-	public function __construct(RequestInterface $request, ResponseInterface $response)
+	public function __construct(ResponseInterface $response)
 	{
-		$this->request = $request;
 		$this->response = $response;
 		parent::__construct();
 	}
 
 	/**
-	 * @param  string    $prefix
-	 * @param  callable  $callback
+	 * @param  ServerRequestInterface  $request
 	 *
 	 * @return void
 	 */
-	public function group(string $prefix, callable $callback): void
+	protected function findRouteNoCached(ServerRequestInterface $request): void
 	{
-		$previousGroup = $this->group;
-		$this->group = trim($prefix, '/');
-		$callback($this);
-		$this->group = $previousGroup;
-	}
+		$currentNode = $this->findRoute($request->getUri()->getPath());
+		$response = $this->response;
+		$handler = $currentNode['handler'] ?? null;
+		$middlewares = $currentNode['middlewares'] ?? [];
+		$params = $currentNode['parameters'] ?? [];
 
-	/**
-	 * Builds the full path for a route considering the current group prefix.
-	 *
-	 * @param  string  $path  The path for the route.
-	 *
-	 * @return string The full path including the group prefix (if any).
-	 */
-	private function buildFullPath(string $path): string
-	{
-		$prefix = $this->group ? '/' . $this->group . '/' : '';
-		return trim($prefix . trim($path, '/'), '/');
-	}
-
-	/**
-	 * @param  string    $method
-	 * @param  string    $path
-	 * @param  callable  $handler
-	 *
-	 * @return void
-	 */
-	public function map(string $method, string $path, callable $handler): void
-	{
-		$method = strtoupper($method);
-		if($method === $this->request->getMethod() && HttpMethod::isValid($method)) {
-			$this->addRoute($this->buildFullPath($path), $handler);
-		}
-
-		if(!HttpMethod::isValid($method)) {
-			throw new RuntimeException('HTTP method not supported');
-		}
-	}
-
-	/**
-	 * @param  string    $path
-	 * @param  callable  $handler
-	 *
-	 * @return void
-	 */
-	public function get(string $path, callable $handler): void
-	{
-		$this->map('GET', $path, $handler);
-	}
-
-	/**
-	 * @param  string    $path
-	 * @param  callable  $handler
-	 *
-	 * @return void
-	 */
-	public function post(string $path, callable $handler): void
-	{
-		$this->map('POST', $path, $handler);
-	}
-
-	/**
-	 * @param  string    $path
-	 * @param  callable  $handler
-	 *
-	 * @return void
-	 */
-	public function put(string $path, callable $handler): void
-	{
-		$this->map('PUT', $path, $handler);
-	}
-
-	/**
-	 * @param  string    $path
-	 * @param  callable  $handler
-	 *
-	 * @return void
-	 */
-	public function delete(string $path, callable $handler): void
-	{
-		$this->map('DELETE', $path, $handler);
-	}
-
-	/**
-	 * @return void
-	 */
-	public function run(): void
-	{
-		$handler = $this->findRoute($this->request->getUri()->getPath());
-
-		if($handler instanceof \Closure) {
-			$handler->call($this, $this->request, $this->response);
-		}
-
-		if(is_null($handler)) {
+		if(is_null($currentNode) || is_null($handler)) {
 			http_response_code(404);
+			return;
 		}
+
+		if(!$handler instanceof \Closure) {
+			return;
+		}
+
+		$dispatcherHandler = new Dispatcher($handler, $response, $params);
+		if(is_array($middlewares) && $middlewares) {
+			foreach(array_reverse($middlewares) as $middleware) {
+				$dispatcherHandler = new MiddlewareDispatcher($middleware, $dispatcherHandler);
+			}
+
+			$dispatcherHandler->handle($request);
+			return;
+		}
+
+		$dispatcherHandler->handle($request);
 	}
+
 }
